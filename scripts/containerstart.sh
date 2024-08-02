b6c4be113010c06ca70d56709da156e41d2a6ed8
Vanashree Pinjari <141754733+van08p@users.noreply.github.com>
2024-08-02 22:46:59 +0530

    Update containerstart.sh

--- scripts/containerstart.sh
+++ scripts/containerstart.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull vishwask22/simple-python-flask-app
+docker pull vanashree08/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:8000 vishwask22/simple-python-flask-app
+docker run -d -p 5000:8000 vanashree08/simple-python-flask-app
