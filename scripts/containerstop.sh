46d75b7b66f3e97c16bb04bbbc6b8153fe165336
Vanashree Pinjari <141754733+van08p@users.noreply.github.com>
2024-08-02 22:50:15 +0530

    Update containerstop.sh

--- scripts/containerstop.sh
+++ scripts/containerstop.sh
@@ -2,5 +2,9 @@
 set -e
 
 # Stop the running container
-cid='docker ps | awk -F " " '{print $1}''
-docker rm -f $cid
+cid=$(docker ps -q)  # `-q` gives only the container IDs
+if [ -n "$cid" ]; then
+  docker rm -f $cid
+else
+  echo "No running containers to stop."
+fi
